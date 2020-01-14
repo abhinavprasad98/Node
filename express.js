cdf22f6905fe51aaa4e88644c4e0c99d2cd412c5
abhinavprasad98 <42972478+abhinavprasad98@users.noreply.github.com>
2020-01-14 09:37:10 +0530

    Add files via upload

--- express.js
+++ express.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
-   res.send('This is the homepage')
-});
+//    res.sendFile(__dirname + '/contact.html');
+res.render('index');
+ });
 app.get('/contact', function(req, res) {
-   res.send('This is the contactpage')
+   //res.send('This is the contactpage');
+   res.render('contact');
+});
+app.get('/profile/:name', function(req, res) {
+   var data = {Age: 20, Job: 'Employee', Hobbies: ['Cricket', 'Piano', 'Reading Books']};
+   res.render('profile', {Person: req.params.name, data: data});
 });
 app.listen(1234);
