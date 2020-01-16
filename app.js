4ccfdaee5282033cfe570309ab88d112d0b85122
abhinavprasad98 <42972478+abhinavprasad98@users.noreply.github.com>
2020-01-16 13:34:00 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-setTimeout(function(){
-    console.log('3 seconds have passed');
-},3000);
-var time = 0;
-var timer = setInterval(function(){
-    time += 2;
-    console.log(time + ' seconds have passed');
-    if(time > 7){
-        clearInterval(timer);
-    }
-}, 2000);
-console.log(__filename);
+var express = require('express');
+var todoController = require(__dirname + '/todoController');
+var app = express();
 
+//set up template engine
+app.set('view engine', 'ejs');
+
+//static files
+app.use(express.static('./Node'));
+
+//fire controllers
+todocontroller(app);
+
+//listen to a port
+app.listen(3000);
+console.log('You are listening to port 3000');
