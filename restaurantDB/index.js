cbd4a305cd4c09e99a34cb277bcf904d148d4a1f
abhinavprasad98 <42972478+abhinavprasad98@users.noreply.github.com>
2020-01-30 10:26:17 +0530

    Create index.js

---
+++ restaurantDB/index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = express.Router();
+
+const environment = process.env.NODE_ENV;
+const stage = ('/config')[environment];
+
+//const routes = require('/routes/index.js');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+const connUri = process.env.MONGO_LOCAL_CONNECTION_URL;
+mongoose.connect(connUri, {
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+
+// if(environment !== 'production') {
+//     app.use(logger('dev'));
+// }
+
+//app.use('/api/rdb', routes(router));
+app.post('/api/rdb', function (req, res, next) {
+    console.log("===========");
+    data = {
+        name:"Abhinav"
+    }
+
+    res.status(200).json({
+        data:data
+    });
+    next();
+});
+
+app.listen(3000, () => {
+   console.log(`started ${3000}`);
+}); 
